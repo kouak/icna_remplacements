d15d91aa41f3f951c97417293131c2616aac1909
Benjamin Beret <benjamin.beret@gmail.com>
2013-07-12 23:30:27 +0200

    User factory + team

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+
 FactoryGirl.define do
   factory :user do
+    name 'NOM'
+    first_name 'Prenom'
+    surname 'Surnom'
+    detailed false
+    email 'example@example.com'
+    password 'please'
+    password_confirmation 'please'
+    # required if the Devise Confirmable module is used
+    confirmed_at Time.now
+    
+    association :team, :factory  => :team
   end
 end
