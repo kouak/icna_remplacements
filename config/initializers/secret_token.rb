b15636f0505dc6b0fe16af7e84957ad28d6c218d
Benjamin Beret <benjamin.beret@gmail.com>
2013-07-12 22:51:11 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LfeeRemplacements::Application.config.secret_key_base = '56312ec26738edec5c1f75a1343d5694162b56eb0563795fdf8c62ef819b82e7bcbb4423eea17b55296d5cc49233d98c2853ad87a67845e0839d0a5605ec547a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LfeeRemplacements::Application.config.session_store :cookie_store, key: '_lfee_remplacements_session'
