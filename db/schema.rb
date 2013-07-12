3234c63efcb3258c265e8eab84dc8ffbb72c78b8
Benjamin Beret <benjamin.beret@gmail.com>
2013-07-12 23:30:05 +0200

    Added teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130712211345) do
+ActiveRecord::Schema.define(version: 20130712212835) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20130712211345) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], name: "index_roles_on_name"
+
+  create_table "teams", force: true do |t|
+    t.integer  "team"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
