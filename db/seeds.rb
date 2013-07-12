e07e31953404e5f37d1d1e66202dd64d9d643998
Benjamin Beret <benjamin.beret@gmail.com>
2013-07-13 00:15:10 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'ADDING DEFAULT TEAM DATA'
+if Team.all.count == 0
+  (1..12).each do |i|
+    Team.create!(:team => i.to_i)
+  end
+end
+
+
+puts 'SETTING UP DEFAULT USER LOGIN'
+
+if User.all.count == 0
+  user = User.create! :first_name => 'First', :name => 'User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => Time.now.utc, :team => Team.find(1), :detailed => false
+  puts 'New user created: ' << user.to_s
+else
+  puts 'Found existing users, aborting'
+end
