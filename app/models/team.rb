4fb3d34a08051d4527cc8278711d59aa5a02982b
Benjamin Beret <benjamin.beret@gmail.com>
2013-07-14 14:23:03 +0200

    Team set_cycle_seed saves correct value (and testing)

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,28 @@ class Team < ActiveRecord::Base
   def to_s
 		self.team.to_s
 	end
+
+  def name
+    to_s
+  end
+
+  # TODO: remove hardcoding
+  def cycle
+    ['M1', 'J', 'S1', false, false, false, 'M2', 'S2', 'N', false, false, false]
+  end
+
+  # Takes a hash as argument
+  # :day_in_cycle => 0 for M1, 1 for J etc
+  # :date => date for this day
+  def set_cycle_seed(args = {})
+    day_in_cycle = args[:day_in_cycle] || 0
+    date = args[:date]
+    # raise an error if arguments are not valid
+    raise Error if !date.is_a?(Date) || cycle[day_in_cycle].nil?
+    # Set first_day_in_cycle correctly
+    self.first_day_in_cycle = date-day_in_cycle
+    # save model
+    save!
+    self
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -9,6 +9,11 @@ describe Team do
     }
   end
 
+  it "should have a correct name" do
+    team = Team.new(@attr.merge(:team => '11'))
+    team.name.should eql('11')
+    team.to_s.should eql('11')
+  end
 
   it "should reject invalid first_day_in_cycle" do
     first_day_in_cycle = ['user@foo.com', nil, '123548']
@@ -24,5 +29,55 @@ describe Team do
       team = Team.new(@attr.merge(:first_day_in_cycle => t))
       team.should be_valid
     end
+  end
+
+  it "should have a valid cycle" do
+    target = ['M1', 'J', 'S1', false, false, false, 'M2', 'S2', 'N', false, false, false]
+    team = Team.new(@attr)
+    team.cycle.should eql(target)
+  end
+
+  it "should refuse to set cycle with invalid arguments" do
+    invalid = [
+      # [:day_in_cycle, :date]
+      [nil, nil],
+      [nil, 'invalid'],
+      [nil, 1],
+      ['invalid', nil],
+      [1, nil],
+      [15, Date.today]
+    ]
+
+    invalid.each do |i|
+      expect { Team.new(@attr).set_cycle_seed(:day_in_cycle => i[0], :date => i[1]) }.to raise_error
+    end
+  end
+
+  it "should set cycle with valid arguments" do
+    valid = { :day_in_cycle => 0, :date => Date.today }
+    team = Team.new(@attr)
+    expect { team.set_cycle_seed(valid) }.to_not raise_error
+  end
+
+  it "should correctly set_cycle_seed" do
+    inputs = [
+      # [ day_in_cycle, date ]
+      [ 0, Date.today ], # M1 today
+      [ 6, Date.today ], # M2 today
+      [ 8, Date.new(2013, 7, 14)] # N on July 14th 2013
+    ]
+    results = []
+    
+    targets = [
+      Date.today, # Today
+      Date.today - 6, # Six days ago
+      Date.new(2013, 7, 6) # M1 on July 6th 2013
+    ]
+
+    inputs.each do |i|
+      results << Team.new(@attr).set_cycle_seed(:date => i[1], :day_in_cycle => i[0]).first_day_in_cycle
+    end
+
+    results.should eql(targets)
   end
 end
