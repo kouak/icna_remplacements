b0a06397ff3b8c55524e6ffe5a60839d1c353053
Benjamin Beret <benjamin.beret@gmail.com>
2013-07-20 00:16:41 +0200

    Popover comments

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
       }],
       eventRender: function (event, element, view) {
         element.popover({
-          placement: 'right',                    
+          placement: 'right',
           title: event.title,
           content: 'text',
-          container: 'body'                               
+          container: 'body' // This is needed to make popovers visible outside the boundaries of the container
         });
 
-        $('body').on('click', function (e) {
+        $('body').on('click', function (e) { // This magic is here to remove popovers when a new one appears
           if (!element.is(e.target) && element.has(e.target).length === 0 && $('.popover').has(e.target).length === 0)
             element.popover('hide');
         });
