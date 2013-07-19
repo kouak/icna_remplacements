6a6193ac7fa416c712e894ff4337a372308d5a49
Benjamin Beret <benjamin.beret@gmail.com>
2013-07-20 00:15:24 +0200

    Calendar popover

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -6,7 +6,20 @@ $(document).ready(function() {
       editable: false,
       eventSources: [{
         url: '/events.json'
-      }]
+      }],
+      eventRender: function (event, element, view) {
+        element.popover({
+          placement: 'right',                    
+          title: event.title,
+          content: 'text',
+          container: 'body'                               
+        });
+
+        $('body').on('click', function (e) {
+          if (!element.is(e.target) && element.has(e.target).length === 0 && $('.popover').has(e.target).length === 0)
+            element.popover('hide');
+        });
+      }
     },
     fullcalendarfr_Options)
   );
